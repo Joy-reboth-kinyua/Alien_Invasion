21b7a3c7d5255317155c9566944cf5426c7a92f6
Joy Reboth <135380604+Joy-reboth-kinyua@users.noreply.github.com>
2025-01-16 13:25:01 -0800

    Merge remote-tracking branch 'origin/master'

--- Alien invasion game.py
+++ Alien invasion game.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -17,11 +18,12 @@ def run_game():
 
 # Start the main loop for the game.
     while True:
+        gf.check_events()
 
  # Watch for keyboard and mouse events.
-       for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-         sys.exit()
+        for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+           sys.exit()
 
        screen.fill(ai_settings.bg_color)
        ship.blitme()
